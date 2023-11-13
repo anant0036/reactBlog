c1f781c86b9e41a02a3f6ea79cdca7b63c04bde3
anant0036 <anant0036@gmail.com>
2023-11-13 22:56:32 +0530

    Store and Rudux Configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { Footer, Header } from './components'
 import './App.css'
 
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline text-rose-700">Bhushan</h1>
-    </>
-  )
+
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          //OUTLET
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
